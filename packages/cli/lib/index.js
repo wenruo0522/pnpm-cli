be688c3c43d80b8d8aa30fff4095b1174ffd174a
zhengjm <376567610@qq.com>
2023-07-17 19:21:52 +0800

    commit: add commander

--- packages/cli/lib/index.js
+++ packages/cli/lib/index.js
@@ -1,6 +1,26 @@
 
+import { program } from 'commander'
+
+import pkg from '../package.json' assert { type: 'json' }
+
+
 const entry = (args) => {
-    console.log('index', args)
+
+    program
+        .name(Object.keys(pkg.bin)[0])
+        .usage('<command> [options]')
+        .version(pkg.version)
+        .option('-d --debug', '是否开启调试模式', false)
+
+    program
+        .command('init [name]')
+        .description('init project')
+        .option('-f, --force', '是否强制更新', false)
+        .action((name, opts) => {
+            console.log('init...', name, opts)
+        })
+
+    program.parse(process.argv)
 }
 export {
     entry
