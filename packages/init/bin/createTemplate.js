4242b904f28cfe65789ed752210f603a14f0a50d
zhengjm <376567610@qq.com>
2023-08-02 19:30:50 +0800

    commit: createTemplate

---
+++ packages/init/bin/createTemplate.js
@@ -0,0 +1,27 @@
+const ADD_TEMPLATE = [
+    {
+        name: 'vue3项目模板',
+        npmName: '@pauli-test/template-vue3',
+        version: '1.0.0',
+    },
+    {
+        name: 'react18项目模板',
+        npmName: '@pauli-test/template-react18',
+        version: '1.0.0',
+    },
+]
+
+const ADD_TYPE = [
+    {
+        name: '项目'
+    },
+    {
+        name: '页面'
+    }
+]
+
+const createTemplate = (name, opts) => {
+
+}
+
+export default createTemplate
--- packages/init/bin/index.js
+++ packages/init/bin/index.js
@@ -1,6 +1,6 @@
-
 import { Command } from '@youmayknow/command'
 import { log } from '@youmayknow/utils'
+import createTemplate from './createTemplate.js'
 
 class InitCommand extends Command {
     get command() {
@@ -17,6 +17,10 @@ class InitCommand extends Command {
 
     action([name, opts]) {
         log.verbose('init', name, opts)
+        // 1. 选择项目模板，生成项目信息
+        createTemplate(name, opts)
+        // 2. 下载项目模板至缓存目录
+        // 3. 安装项目模板至项目目录
     }
 }
 
